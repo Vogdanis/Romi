8b44eea9cf0be3b0505c7d66469c76f4f181caf2
Ubuntu User <yvogdanis@gmail.com>
2019-05-10 12:57:34 +0200

    First commit

---
+++ cableBotMovement.py
@@ -0,0 +1,70 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+# Motor Pins and Variables
+motorDirPin = 20
+motorStepPin = 21
+motorDir = 0
+motorStepsPerRev = 200 
+
+# EndStops 
+endstopHomePin = 26
+endstopEndPin = 19
+homeReached = 0
+
+def homeEndstopReached():
+    homeReached = GPIO.input(endstopHomePin)
+    if not homeReached:
+	print "HomeReached"
+        return 1	
+
+
+def seekHome():
+    while homeReached is 0:
+	motorStep()
+	if  homeEndstopReached():
+            break;
+
+def motorStep():
+    GPIO.output(motorStepPin, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(motorStepPin, GPIO.LOW)
+    sleep(delay)
+
+def changeDir():
+    global motorDir
+    motorDir = not motorDir
+    GPIO.output(motorDirPin, motorDir)
+
+def debounceFromEndstop(steps):
+    sleep(1)
+    print "Debounce from endstop"
+    changeDir()
+    print motorDir
+    for i in range(steps):
+	motorStep()
+
+
+print("CableBot v0.1")
+
+#Setup GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(motorDirPin, GPIO.OUT)
+GPIO.setup(motorStepPin, GPIO.OUT)
+GPIO.setup(endstopHomePin, GPIO.OUT)
+GPIO.setup(endstopEndPin, GPIO.OUT)
+
+GPIO.output(motorDirPin, motorDir)
+print "Motor dir = " + str(motorDir)
+
+print "EndstopHome = " + str(GPIO.input(endstopHomePin))
+print "EndstopEnd = " + str(GPIO.input(endstopEndPin))
+
+delay = .001
+
+seekHome()
+debounceFromEndstop(100)
+
+
+GPIO.cleanup()
